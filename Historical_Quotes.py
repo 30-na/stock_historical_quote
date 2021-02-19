b45428e03c764b1ad8d6dcd83f3556b217dc675b
sina <78888004+30-na@users.noreply.github.com>
2021-02-19 14:24:37 -0600

    Add files via upload

--- Historical_Quotes.py
+++ Historical_Quotes.py
@@ -41,50 +41,78 @@ remove_header_turn_to_float(low)
 
 f.close()
 
-# rolling average: standard deviation in 5 days windows
-def RA_5(close):
+# Rolling Average: standard deviation different period time window
+def RA(close, timePeriod):
     RA = []
-    i = 0
-    while i+4 < len(close):
+    for i in range(len(close) - timePeriod + 1):
         sum = 0
-        for j in range(5):
+        for j in range(timePeriod):
             sum = close[i+j] + sum          
-        RA.append(sum/5)
-        i += 1 
-    return RA
-
-
-# rolling average: standard deviation in 5 days windows
-def RA_10(close):
-    RA = []
-    i = 0
-    while i+9 < len(close):
-        sum = 0
-        for j in range(10):
-            sum = close[i+j] + sum
-        RA.append(sum/10)
-        i += 1
+        RA.append(sum/timePeriod)
     return RA
 
 
 
-#MACD moving average convergence divergence: a display trend following characteristic and momentum chararacteristic
-def MACd(close):
-    
-    
-    def EMA_12day(close):
-        EMA = []
-        #the first value = a trailing 12 days average
+# Moving Average Convergence Divergence: a display trend following characteristic and momentum chararacteristic
+def MACD(close, timePeriodShort = 12, timePeriodLong = 26):
+    def EMA(close, timePeriod):
+        EMA = []     
+        #the first day = a trailing 12 days average
         sum = 0
-        for j in range(1,13):
+        for j in range(1,(timePeriod+1) ):
             sum = close[len(close)-j] + sum
-        EMA.append(sum/12)
-        # EMA formula:
-        for i in range (13 , len(close))
-            
-        EMA.append(close)
-        
+        EMA.append(sum/timePeriod)   
+        # EMA formula after the first day:
+        for i in range ((timePeriod+1) , len(close) + 1):
+            EMA.append( (close[len(close) - i])*(2/(timePeriod+1)) + (EMA[i-(timePeriod+1)])*(1 - ( 2/ (timePeriod+1) ) ) )  
+        # reverce the EMA                
+        EMA.reverse()      
+        return EMA
+    EMA_short = EMA(close, timePeriodShort)
+    EMA_long = EMA(close, timePeriodLong)
+    MACD = []    
+    for i in range(len(EMA_long)):
+        MACD[i] = EMA_short[i] - EMA_long[i]
+    return MACD
 
-print (close)
-print (close[1])
-print (close[len(close)-1])
+# Commodity Change: an identification of cyclinical trend
+def CCI(high, low, close, timePeriod = 20):
+    constant = .015    
+    #Typical Price = (TP) = (High + Low + Close)/3
+    def TP(high, low, close):
+        typicalPrice = []
+        for i in range(len(close)):
+            typicalPrice.append ( ( high[i] + low[i] + close[i] ) / 3)
+        return typicalPrice
+    #Simple Moving Average of Typical Price = (High + Low + Close)/3
+    def SMA_TP(typicalPrice, timePeriod = 20):
+        SMA = []
+        for i in range((len(typicalPrice) - timePeriod) + 1):
+            sum = 0
+            for j in range(timePeriod):
+                sum = typicalPrice[i+j] + sum          
+            SMA.append(sum/timePeriod)
+        return SMA
+    # Mean Deviation
+    def MD(typicalPrice, simpleMovingAverage, timePeriod = 20):
+        MD = []
+        for i in range(len(simpleMovingAverage)):
+            sum = 0
+            for j in range (timePeriod):
+                sum = abs(simpleMovingAverage[i] - typicalPrice[i+j]) +  sum
+            MD.append(sum/timePeriod)
+        return MD
+    # CCI = (Typical Price  -  Time Period SMA of TP) / (.015 x Mean Deviation)
+    
+    typicalPrice = TP (high, low, close)
+    simpleMovingAverage  = SMA_TP (typicalPrice, timePeriod = 20)
+    meanDeviation = MD (typicalPrice, simpleMovingAverage, timePeriod = 20)
+    CCI = []
+    for i in range(len(close) - timePeriod + 1):
+        CCI.append ((typicalPrice[i] - simpleMovingAverage[i]) / (constant * meanDeviation[i]))
+    return CCI    
+print (CCI(high, low, close))       
+        
+        
+    
+    
