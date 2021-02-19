2a84001f7effc5c8ec45dc13f7a5109ddeef0c90
sina <78888004+30-na@users.noreply.github.com>
2021-02-19 09:52:32 -0600

    Add files via upload

--- Historical_Quotes.py
+++ Historical_Quotes.py
@@ -25,7 +25,7 @@ for row in csv_f:
     low.append(row[5])
 
 
-## remove the $ sign and header 
+# remove the $ sign and header 
 def remove_header_turn_to_float(listName):
     listName.pop(0)
     i = 0
@@ -41,5 +41,50 @@ remove_header_turn_to_float(low)
 
 f.close()
 
-high - low  
+# rolling average: standard deviation in 5 days windows
+def RA_5(close):
+    RA = []
+    i = 0
+    while i+4 < len(close):
+        sum = 0
+        for j in range(5):
+            sum = close[i+j] + sum          
+        RA.append(sum/5)
+        i += 1 
+    return RA
+
+
+# rolling average: standard deviation in 5 days windows
+def RA_10(close):
+    RA = []
+    i = 0
+    while i+9 < len(close):
+        sum = 0
+        for j in range(10):
+            sum = close[i+j] + sum
+        RA.append(sum/10)
+        i += 1
+    return RA
+
+
+
+#MACD moving average convergence divergence: a display trend following characteristic and momentum chararacteristic
+def MACd(close):
     
+    
+    def EMA_12day(close):
+        EMA = []
+        #the first value = a trailing 12 days average
+        sum = 0
+        for j in range(1,13):
+            sum = close[len(close)-j] + sum
+        EMA.append(sum/12)
+        # EMA formula:
+        for i in range (13 , len(close))
+            
+        EMA.append(close)
+        
+
+print (close)
+print (close[1])
+print (close[len(close)-1])
